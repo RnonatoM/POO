1f48d1ce224fcade93ec2177b69f3c14f553435b
Raimundo Nonato Maia <112270577+RnonatoM@users.noreply.github.com>
2023-12-11 20:18:29 -0300

    Questão 18

--- Introdução-ao-Java/Q18.java
+++ Introdução-ao-Java/Q18.java
@@ -1,19 +1,21 @@
-public class Q18 {
+import java.util.Scanner;
+public class FatorialRecursiva {
     public static void main(String[] args) {
-        int n = 5;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um número: ");
+        int n = scanner.nextInt();
         
         System.out.println("n = " + n);
         System.out.println("0! = " + fatorial(n, 0));
     }
-
     public static int fatorial(int n, int nivel) {
         if (n == 0) {
             return 1;
         }
-
         System.out.print("\t".repeat(nivel) + n + "! = ");
         int resultado = n * fatorial(n - 1, nivel + 1);
         System.out.println(resultado);
         return resultado;
     }
 }
+
